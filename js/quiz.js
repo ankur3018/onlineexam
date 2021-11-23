743378c05a7706708b2c32550799bb25d9c6a1a9
ankur3018 <kumarankur3018@gmail.com>
2021-11-23 22:14:18 +0530

    last commit

---
+++ js/quiz.js
@@ -0,0 +1,54 @@
+let quiz=[
+    {
+        question:"Where is the headquarter of the ICC?",
+        option:[
+            "1.London",
+            "2.Cape town",
+            "3.Sydney",
+            "4.Dubai",
+        ],
+        answer:4,
+    },
+    {
+        question:"Which of the following international competition is not organized by the ICC?",
+        option:[
+            "1.Champions Trophy",
+            "2.World Cricket League",
+            "3.Under-19 Cricket World Cup",
+            "4.Syed Mushtaq Ali Trophy",
+            
+        ],
+        answer:4,
+    },
+    {
+        question:"Indian played their First T20 Match against _______.",
+        option:[
+            "1.South Africa",
+            "2.USA",
+            "3.Pakistan",
+            "4.England",   
+        ],
+        answer:1,
+    },
+    {
+        question:"Who is the current coach of the Indian Team",
+        option:[
+            "1.Sachin Tendulkar",
+            "2.Suresh Raina",
+            "3.Rahul Dravid",
+            "4.Vinod kambli",   
+        ],
+        answer:3,
+
+    },
+    {
+        question:"Indian played first test match against ________ .",
+        option:[
+            "1.England",
+            "2.Sri Lanka",
+            "3.China",
+            "4.Pakistan",   
+        ],
+        answer:1,
+    }
+]
